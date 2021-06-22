53ad6841e7e209dd86880e263364da51f8b2df80
Ruslan Wanandi <rwanandi030500@gmail.com>
2021-06-22 08:02:01 +0700

    Update config.php
    update config

--- server/application/config/config.php
+++ server/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/MyResume/';
+$config['base_url'] = 'https://ruslan03.github.io/my-resume/';
 
 /*
 |--------------------------------------------------------------------------
